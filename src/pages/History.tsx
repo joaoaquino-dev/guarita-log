da3426c5842bae3a5a755555393bb4f35e7bf067
joaoquino-dev <joaoaquino.dev@outlook.com>
2026-04-07 11:25:11 -0300

    feat: add history page with shift records

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,7 +1,28 @@
+import { getRecords } from "../utils/storage";
+import { useNavigate } from "react-router-dom";
+
 const History = () => {
+  const navigate = useNavigate();
+  const records = getRecords();
   return (
     <div>
       <h1>Histórico</h1>
+
+      <div>
+        {records.length > 0 ? (
+          records.map((record) => (
+            <div key={record.id}>
+              <p>{record.guardName}</p>
+              <p>{record.date}</p>
+              {record.notes && <p>{record.notes}</p>}
+            </div>
+          ))
+        ) : (
+          <p>Nenhum registro encontrado!</p>
+        )}
+      </div>
+
+      <button onClick={() => navigate("/")}>Voltar ao início</button>
     </div>
   );
 };
