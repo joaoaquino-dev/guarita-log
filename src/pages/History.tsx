de242608488a36b966306eba7e70b3522f880823
joaoquino-dev <joaoaquino.dev@outlook.com>
2026-04-07 12:11:27 -0300

    style: add base styling to history page

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -5,24 +5,37 @@ const History = () => {
   const navigate = useNavigate();
   const records = getRecords();
   return (
-    <div>
-      <h1>Histórico</h1>
+    <div
+      className="bg-gray-900 flex flex-col
+    items-center min-h-screen p-4"
+    >
+      <h1 className="font-bold text-4xl text-white mb-4">Histórico</h1>
 
-      <div>
+      <div className="w-full mt-4">
         {records.length > 0 ? (
           records.map((record) => (
-            <div key={record.id}>
-              <p>{record.guardName}</p>
-              <p>{record.date}</p>
-              {record.notes && <p>{record.notes}</p>}
+            <div
+              key={record.id}
+              className="bg-gray-800 p-4 rounded-lg w-full mb-3"
+            >
+              <p className="text-white font-bold">{record.guardName}</p>
+              <p className="text-gray-400 text-sm">{record.date}</p>
+              {record.notes && (
+                <p className="text-gray-300 mt-2 text-sm">{record.notes}</p>
+              )}
             </div>
           ))
         ) : (
-          <p>Nenhum registro encontrado!</p>
+          <p className="text-white">Nenhum registro encontrado!</p>
         )}
       </div>
 
-      <button onClick={() => navigate("/")}>Voltar ao início</button>
+      <button
+        className="bg-gray-600 text-white px-4 py-3 rounded-md"
+        onClick={() => navigate("/")}
+      >
+        Voltar ao início
+      </button>
     </div>
   );
 };
